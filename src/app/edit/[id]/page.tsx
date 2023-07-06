5089756dd852b6309e3b1811cbb14aa7b0f7db4e
João Paulo Costa <kenocosta44@gmail.com>
2023-07-06 00:15:40 -0300

    feat: update edit implementation

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,89 +1,106 @@
 "use client";
 
-import {
-  Button,
-  ChakraProvider,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
-import { Field, Form, Formik } from "formik";
-import { UserModel } from "../../models/user.model";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import { UserModel } from "../../../models/user.model";
 import { useEffect, useState } from "react";
+import { FormField } from "@/components/FormField";
+import "./styles.scss";
+import { BreadCrumb } from "@/components/BreadCrumb";
+import { BreadCrumbModel } from "@/models/breadcrumb.model";
+import { useRouter } from "next/navigation";
 
-export default function Create(args:{params: { id: number}}) {
+export default function Edit(args: { params: { id: number } }) {
   const [user, setUser] = useState<any>();
+  const [breadcrumb, setBreacrumb] = useState<BreadCrumbModel[]>([]);
+  const router = useRouter();
 
   const handleSubmit = (user: UserModel) => {
-    const data = JSON.parse(localStorage.getItem("users") ?? '[]') as UserModel[];
-    const userIndex = data.findIndex(item => item.id === +args.params.id);
+    const data = JSON.parse(
+      localStorage.getItem("users") ?? "[]"
+    ) as UserModel[];
+    const userIndex = data.findIndex((item) => item.id === +args.params.id);
     data[userIndex] = user;
-    localStorage.setItem('users', JSON.stringify(data));
+    localStorage.setItem("users", JSON.stringify(data));
+    router.push('/');
   };
-  useEffect(()=>{
-    const users = JSON.parse(localStorage.getItem('users') ?? '[]') as UserModel[];
-    const _user = users.find(user => user.id === +args.params.id);
-    console.log(_user);
+
+  useEffect(() => {
+    const users = JSON.parse(
+      localStorage.getItem("users") ?? "[]"
+    ) as UserModel[];
+    const _user = users.find((user) => user.id === +args.params.id);
     setUser(_user);
-  }, [args.params.id])
+    setBreacrumb([
+      {
+        id: 1,
+        name: "Home",
+        url: "/",
+      },
+      {
+        id: 2,
+        name: _user?.name,
+        isCurrent: true,
+      }
+    ]);
+  }, [args.params.id]);
   
   return (
-    <ChakraProvider>
-      <Formik initialValues={user} onSubmit={handleSubmit} enableReinitialize={true}>
-        {(props) => (
-          <Form>
-            <Field name="name">
-              {({ field, form }: { field: any; form: any }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel>Name</FormLabel>
-                  <Input {...field} placeholder="name" />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Field name="username">
-              {({ field, form }: { field: any; form: any }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel>User name</FormLabel>
-                  <Input {...field} placeholder="user name" />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Field name="email">
-              {({ field, form }: { field: any; form: any }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel>Email</FormLabel>
-                  <Input
-                    {...field}
-                    placeholder="email@email.com"
-                    type="email"
-                  />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Field name="phone">
-              {({ field, form }: { field: any; form: any }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel>Phone</FormLabel>
-                  <Input {...field} placeholder="(00) 0000-0000" type="text" />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Button
-              mt={4}
-              colorScheme="teal"
-              isLoading={props.isSubmitting}
-              type="submit"
-            >
-              Submit
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </ChakraProvider>
+    <section id="container">
+      <BreadCrumb segments={breadcrumb} />
+      <ChakraProvider>
+        <Formik
+          initialValues={user}
+          onSubmit={handleSubmit}
+          enableReinitialize={true}
+          validateOnChange={false}
+          validateOnBlur={true}
+        >
+          {(props) => (
+            <Form>
+              <FormField
+                name="name"
+                label="Name"
+                placeholder="name"
+                type="text"
+                isRequired={true}
+                errorMessage="Nome é obrigatório"
+              />
+              <FormField
+                name="username"
+                label="User name"
+                placeholder="user name"
+                isRequired={true}
+                errorMessage="User name é obrigatório"
+              />
+              <FormField
+                name="email"
+                label="Email"
+                placeholder="email@email.com"
+                type="email"
+                isRequired={true}
+                errorMessage="Email é obrigatório"
+              />
+              <FormField
+                name="phone"
+                label="Phone"
+                placeholder="(00) 0000-000"
+                type="text"
+                isRequired={true}
+                errorMessage="Telefone é obrigatório"
+              />
+              <Button
+                mt={4}
+                colorScheme="teal"
+                isLoading={props.isSubmitting}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </ChakraProvider>
+    </section>
   );
 }
