74dd8bc91705528a811246ed4785a2a3e8d37c1a
João Paulo Costa <kenocosta44@gmail.com>
2023-07-06 00:14:22 -0300

    feat: update add implementation

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,27 +1,43 @@
 "use client";
 
-import {
-  Button,
-  ChakraProvider,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
-import { Field, Form, Formik } from "formik";
-import { UserModel } from "../models/user.model";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import { UserModel } from "../../models/user.model";
+import { FormField } from "../../components/FormField";
+import "./styles.scss";
+import { BreadCrumb } from "@/components/BreadCrumb";
+import { BreadCrumbModel } from "@/models/breadcrumb.model";
+import { useRouter } from "next/navigation";
 
-export default function Create() {
+export default function Add() {
+  const router = useRouter();
+  const breadcrumb: BreadCrumbModel[] = [
+    { 
+      id: 1,
+      name: "Home",
+      url: "/",
+    },
+    { 
+      id: 2,
+      name: "New user",
+      url: "/add",
+      isCurrent: true,
+    },
+  ];
+
   const handleSubmit = (user: UserModel) => {
     const data = localStorage.getItem("users");
-    if (data) {
-      const users = JSON.parse(data) as UserModel[];
-      users.push(user);
-      localStorage.setItem('users', JSON.stringify(users));
-      return;
-    }
-    localStorage.setItem('users', JSON.stringify([user]));
+    const users = JSON.parse(data ?? "[]") as UserModel[];
+    const maxId = getMaxId(users);
+    users.push({...user, id: maxId + 1});
+    localStorage.setItem("users", JSON.stringify(users));
+    router.push('/');
   };
+
+  const getMaxId = (users: UserModel[]) =>{
+    return Math.max(...users.map(user => user.id ?? 0));
+  }
+
   const initialValues = {
     name: "",
     username: "",
@@ -31,61 +47,61 @@ export default function Create() {
   };
 
   return (
-    <ChakraProvider>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        {(props) => (
-          <Form>
-            <Field name="name">
-              {({ field, form }: { field: any; form: any }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel>Name</FormLabel>
-                  <Input {...field} placeholder="name" />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Field name="username">
-              {({ field, form }: { field: any; form: any }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel>User name</FormLabel>
-                  <Input {...field} placeholder="user name" />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Field name="email">
-              {({ field, form }: { field: any; form: any }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel>Email</FormLabel>
-                  <Input
-                    {...field}
-                    placeholder="email@email.com"
-                    type="email"
-                  />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Field name="phone">
-              {({ field, form }: { field: any; form: any }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel>Phone</FormLabel>
-                  <Input {...field} placeholder="(00) 0000-0000" type="text" />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Button
-              mt={4}
-              colorScheme="teal"
-              isLoading={props.isSubmitting}
-              type="submit"
-            >
-              Submit
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </ChakraProvider>
+    <section id="container">
+      <BreadCrumb segments={breadcrumb} />
+      <ChakraProvider>
+      <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          enableReinitialize={true}
+          validateOnChange={false}
+          validateOnBlur={true}
+        >
+          {(props) => (
+            <Form>
+              <FormField
+                name="name"
+                label="Name"
+                placeholder="name"
+                type="text"
+                isRequired={true}
+                errorMessage="Nome é obrigatório"
+              />
+              <FormField
+                name="username"
+                label="User name"
+                placeholder="user name"
+                isRequired={true}
+                errorMessage="User name é obrigatório"
+              />
+              <FormField
+                name="email"
+                label="Email"
+                placeholder="email@email.com"
+                type="email"
+                isRequired={true}
+                errorMessage="Email é obrigatório"
+              />
+              <FormField
+                name="phone"
+                label="Phone"
+                placeholder="(00) 0000-000"
+                type="text"
+                isRequired={true}
+                errorMessage="Telefone é obrigatório"
+              />
+              <Button
+                mt={4}
+                colorScheme="teal"
+                isLoading={props.isSubmitting}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </ChakraProvider>
+    </section>
   );
 }
